e3f4449d72a22cb4c044c96e9a38b075e6c0bdb7
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-10-22 01:22:58 +0300

    add script

--- src/js/countdown-timer.js
+++ src/js/countdown-timer.js
@@ -1,46 +1,81 @@
+const refs1 = {
+  days: document.querySelector('#timer-1 [data-value="days"]'),
+  hours: document.querySelector('#timer-1 [data-value="hours"]'),
+  mins: document.querySelector('#timer-1 [data-value="mins"]'),
+  secs: document.querySelector('#timer-1 [data-value="secs"]'),
+};
+
+const refs2 = {
+  days: document.querySelector('#timer-2 [data-value="days"]'),
+  hours: document.querySelector('#timer-2 [data-value="hours"]'),
+  mins: document.querySelector('#timer-2 [data-value="mins"]'),
+  secs: document.querySelector('#timer-2 [data-value="secs"]'),
+};
+
 class CountdownTimer {
-  constructor({ selector, targetDate }) {
+  constructor({ selector, targetDate, onTick }) {
+    this.intervalId = null;
     this.selector = selector;
     this.targetDate = targetDate;
+    this.onTick = onTick;
+  }
+
+  start() {
+    this.intervalId = setInterval(() => {
+      const finishTime = this.targetDate;
+
+      const currentTime = Date.now();
+      const deltaTime = finishTime - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      this.onTick(time);
+
+      if (finishTime <= currentTime + 1000) {
+        clearInterval(this.intervalId);
+      }
+    }, 1000);
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
   }
 }
 
-const refs = {
-  days: document.querySelector('[data-value="days"]'),
-  hours: document.querySelector('[data-value="hours"]'),
-  minutes: document.querySelector('[data-value="minutes"]'),
-  seconds: document.querySelector('[data-value="seconds"]'),
-};
+function updateTimerFace1({ days, hours, mins, secs }) {
+  refs1.days.textContent = `${days}`;
+  refs1.hours.textContent = `${hours}`;
+  refs1.mins.textContent = `${mins}`;
+  refs1.secs.textContent = `${secs}`;
+}
 
-const timer = new CountdownTimer({
+function updateTimerFace2({ days, hours, mins, secs }) {
+  refs2.days.textContent = `${days}`;
+  refs2.hours.textContent = `${hours}`;
+  refs2.mins.textContent = `${mins}`;
+  refs2.secs.textContent = `${secs}`;
+}
+
+const countdownTimer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+  targetDate: new Date('Oct 22, 2020 10:48:10'),
+  onTick: updateTimerFace1,
 });
 
-const time = targetDate - Date.now();
+const countdownTimer2 = new CountdownTimer({
+  selector: '#timer-2',
+  targetDate: new Date('Oct 22, 2020 01:55:00'),
+  onTick: updateTimerFace2,
+});
 
-console.log(timer);
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+countdownTimer.start();
+countdownTimer2.start();
