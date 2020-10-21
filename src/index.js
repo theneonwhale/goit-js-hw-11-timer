1ab43ff381cff2a6c52df4556ae5eb6f45c40ec6
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-10-21 17:20:12 +0300

    add webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './js/color-switch.js';
+import './js/countdown-timer.js';
---
+++ src/js/colors.js
@@ -0,0 +1,15 @@
+const colors = [
+  '#FFADAD',
+  '#FFD6A5',
+  '#FDFFB6',
+  '#CAFFBF',
+  '#9BF6FF',
+  '#9EDDFF',
+  '#A0C4FF',
+  '#AFBBFF',
+  '#BDB2FF',
+  '#FFC6FF',
+  '#FFFFFC',
+];
+
+export { colors };
